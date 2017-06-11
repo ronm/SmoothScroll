d11fdbba7804e02a9a3844499c68baf01d4a4d0b
Ron <ron@Linkcreative.com>
2017-06-11 14:05:37 -0700

    Updating to ES6 version

--- SmoothScroll.js
+++ SmoothScroll.js
@@ -3,81 +3,68 @@
 /*** by Ron Marcelle ******/
 /*** Licensed under MIT ***/
 /**************************/
-var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol ? "symbol" : typeof obj; };
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-
-(function () {
-
-  var defaults = { speed: 300, offset: null, callback: null };
-
-  var SmoothScroll = function () {
-   function SmoothScroll() {
-      var target = arguments.length <= 0 || arguments[0] === undefined ? null : arguments[0];
-      var opts = arguments.length <= 1 || arguments[1] === undefined ? {} : arguments[1];
-
-      _classCallCheck(this, SmoothScroll);
-
-      var offset = null;
-      this.target = this.startPos = this.start = null;
-      this.targetOrig = target;
-      this.settings = Object.assign(defaults, opts);
-
-      if (offset = this.settings.offset) {
-        this.settings.offset = isNaN(offset) ? this._getElement(offset).clientHeight : Number(offset);
-      }
-
-      this._start();
-    }
-
-    SmoothScroll.prototype._scroll = function _scroll() {
-      var _this = this;
-
-      var percentage = (Date.now() - this.start) / this.settings.speed,
-          be = this.startPos + (this.target - this.startPos) * percentage;
-
-      if (percentage < 1) {
-        window.scrollTo(window.scrollX, be);
-        window.requestAnimationFrame(function () {
-          return _this._scroll();
-        });
-      } else {
-        window.scrollTo(window.scrollX, this.target);
-        if (this.settings.callback) {
-          this.settings.callback.call(this.target);
-        }
-      }
-    };
-
-    SmoothScroll.prototype._start = function _start() {
-      var target = isNaN(this.targetOrig) ? this._getElementPosition(this.targetOrig) - this.settings.offset : Math.max(0, Number(this.targetOrig));
-
-      this.target = target < 0 ? window.pageYOffset + target : target;
-      this.startPos = window.pageYOffset;
-      this.start = Date.now();
-      this._scroll();
-    };
-
-    SmoothScroll.prototype._getElement = function _getElement(el) {
-      return el instanceof HTMLElement ? el : document.querySelector(el);
-    };
-
-    SmoothScroll.prototype._getElementPosition = function _getElementPosition(el) {
-      return Math.floor(this._getElement(el).getBoundingClientRect().top + this.startPos);
-    };
-
-    return SmoothScroll;
-  }();
-
-  if (typeof define === 'function' && _typeof(define.amd) === 'object' && define.amd) {
-    define(function () {
-      return SmoothScroll;
-    });
-  } else if (typeof module !== 'undefined' && module.exports) {
-    module.exports = SmoothScroll.attach;
-    module.exports.SmoothScroll = SmoothScroll;
-  } else {
-    window.SmoothScroll = SmoothScroll;
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        define([], factory);
+    } else if (typeof module === 'object' && module.exports) {
+        module.exports = factory();
+    } else {
+        root.smoothScroll = factory();
   }
-})();
+}(this, function () {
+	
+    let defaults = {
+		speed: 300,
+		offset: null,
+		callback: null,
+	};
+	
+	return class SmoothScroll {
+  
+	    constructor(target = null, opts = {}) {
+	        this.target = null;
+	        this.targetOrig = target;
+	        this.startPos = null;
+	        this.start = null;
+	      
+	        this.settings = Object.assign({}, defaults, opts);
+	
+	        this._start();
+	    }
+	
+	    _scroll() {
+		    var percentage = (Date.now() - this.start) / this.settings.speed,
+				be = this.startPos + ((this.target - this.startPos) * percentage);
+	
+	        if ( percentage < 1 ) {
+				window.scrollTo(window.scrollX, be);
+				window.requestAnimationFrame(() => this._scroll());
+		    } else {
+				window.scrollTo(window.scrollX, this.target);
+				if ( this.settings.callback ) { this.settings.callback.call(this.target); }
+			}
+		}
+	    
+	    _start() {
+		    requestAnimationFrame(() => {
+		      	this.startPos = window.pageYOffset;
+		
+		        var offset = isNaN(this.settings.offset) ? this._getElement(this.settings.offset).clientHeight : Number(this.settings.offset);
+				var target = isNaN(this.targetOrig) ? (this._getElementPosition(this.targetOrig) - offset) : Math.max(0,Number(this.targetOrig)); 
+		
+				this.target = target < 0 ? (window.pageYOffset + target) : target;			
+				this.start = Date.now();
+				this._scroll();
+			});
+	    }
+	  
+	    _getElement(el) { 
+			return el instanceof HTMLElement ? el : document.querySelector(el); 
+	    }
+	  
+	    _getElementPosition(el) {
+			return Math.floor(this._getElement(el).getBoundingClientRect().top + this.startPos);
+	    }
+	}
+}));
